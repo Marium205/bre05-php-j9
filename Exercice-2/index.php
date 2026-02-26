952adfab2cdf2deb9fc0feed55b4c229a68d1c7d
Mariyam Abdul Rauf <marium205@hotmail.com>
2026-02-26 11:25:46 +0000

    Exercice 2 : attributs par défaut et méthodes

---
+++ Exercice-2/index.php
@@ -0,0 +1,13 @@
+<?php
+
+require "Character.php";
+
+$character = new Character(1);
+
+echo $character->getFullName();
+echo "<br>";
+
+$character->setFirstName("Sarah");
+$character->setLastName("Connor");
+
+echo $character->getFullName();
